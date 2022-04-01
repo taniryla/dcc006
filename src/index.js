310b9c73c975fea2c7cc4ac2e9b3d6a3b6a261ad
Ryland Taniguchi <ryland0000@gmail.com>
2022-03-31 18:32:59 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+/*-----------------------------------------------------------------
+Challenge: 06-range
+Difficulty: basic
+Prompt:
+- Write a function called range that accepts two integers as arguments and returns an array of integers starting with the first argument up to one less than the second argument.
+- The range function must be called with the first argument less than or equal to the second argument, otherwise return the string "First argument must be less than second".
+Examples:
+range(1,4) //=> [1,2,3]
+range(-2, 3) //=> [-2,-1,0,1,2]
+range(1,1) //=> []
+range(5,2) //=> "First argument must be less than second"
+-----------------------------------------------------------------*/
+// Your solution for 06-range here:
+
+</div>
+`;
+
+function range (int1, int2) {
+  let arrayInt = [];
+  if (int1 > int2) return "First argument must be less than second";
+  for (let i = int1; i < int2; i++) {
+    arrayInt.push(i);
+  }
+  return arrayInt;
+}
+
+}
